3a43a2525ebf34dfe64eca33dc769e0732af0776
RABE_IT Services <development@berndrabe.de>
2016-04-04 07:26:52 +0200

    Initial Commit

---
+++ Base64/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Base64
+//
+//  Created by Bernd Rabe on 04.04.16.
+//  Copyright © 2016 RABE_IT Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
