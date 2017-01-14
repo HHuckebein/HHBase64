7634c87ea303d8b87f9b46f73215e0509174fa9c
RABE_IT Services <development@berndrabe.de>
2017-01-14 08:30:42 +0100

    Changed to CocoaTouch framework

---
+++ Sources/Base64.h
@@ -0,0 +1,19 @@
+//
+//  Base64.h
+//  Base64
+//
+//  Created by Bernd Rabe on 14.01.17.
+//  Copyright © 2017 RABE_IT Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base64.
+FOUNDATION_EXPORT double Base64VersionNumber;
+
+//! Project version string for Base64.
+FOUNDATION_EXPORT const unsigned char Base64VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base64/PublicHeader.h>
+
+
